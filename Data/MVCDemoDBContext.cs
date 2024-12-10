f2541f816843d4b0746bc07e6c262110ee3bacc5
Syfur Rahaman Shohag <syfur.srs@gmail.com>
2024-12-10 23:18:25 +0600

    first commit

---
+++ Data/MVCDemoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class MVCDemoDBContext : DbContext
+    {
+        public MVCDemoDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
